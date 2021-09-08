5269be02efce9d87213ea2c75d3736745327b77e
alpinetyler <49990880+alpinetyler@users.noreply.github.com>
2021-09-07 19:36:08 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <header>
+            <section class="myname">Tyler P Thompson</section>
+            <section class="menu">
+                <div>SKILLS</div>
+                <div>PROJECTS</div>
+                <div>CONTACT</div>
+             </section>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Top.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function Top() {
+    return (
+        <section class="top">
+            <section class="picture">
+                <img src="../images/BW.jpg" alt="pic of Tyler" />
+            </section>
+            <section class="contactInfo">
+                <span class="headingName">Tyler Thompson</span>
+                <section class="tagline">Web Developer</section>
+                <span class="heading">Phone:</span>
+                <section class="textLine">801-787-5574</section>
+                <span class="heading">Email:</span>
+                <section class="textLine">alpinetyler@gmail.com</section>
+                <span class="heading">Address:</span>
+                <section class="textLine">120 E Center St</section>
+                <section class="textLine">Alpine, UT 84004</section>
+            </section>
+        </section>
+    );
+}
+
+export default Top;
